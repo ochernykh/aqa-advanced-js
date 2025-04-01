5a7d276af3b318831dec6a2ab4f6b418749aaa15
ochernykh <0595887@gmail.com>
2025-04-01 09:49:19 +0000

    Update task02.js

--- homework/03_conditions_loops/task02.js
+++ homework/03_conditions_loops/task02.js
@@ -1,20 +1,25 @@
-const averageGrade = 79;
-const gradeGroup = Math.floor(averageGrade / 10);
+const averageGrade = 85;
 
-switch (gradeGroup) {
-  case 10:
-  case 9:
-    console.log("Відмінно");
-    break;
-  case 8:
-    console.log("Дуже добре");
-    break;
-  case 7:
-    console.log("Добре");
-    break;
-  case 6:
-    console.log("Задовільно");
-    break;
-  default:
-    console.log("Незадовільно");
+if (averageGrade < 0 || averageGrade > 100 || isNaN(averageGrade)) {
+  console.log("Некоректна оцінка");
+} else {
+  const gradeGroup = Math.floor(averageGrade / 10);
+
+  switch (gradeGroup) {
+    case 10:
+    case 9:
+      console.log("Відмінно");
+      break;
+    case 8:
+      console.log("Дуже добре");
+      break;
+    case 7:
+      console.log("Добре");
+      break;
+    case 6:
+      console.log("Задовільно");
+      break;
+    default:
+      console.log("Незадовільно");
+  }
 }
