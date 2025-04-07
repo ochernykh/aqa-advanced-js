65c29eb3efbed508e090a7721b823caead1eb147
ochernykh <0595887@gmail.com>
2025-04-07 20:21:00 +0300

    Add 04_functions_basics homework tasks 01–03

---
+++ homework/04_functions_basics/task03.js
@@ -0,0 +1,15 @@
+function checkOrder(available, ordered) {
+    if (ordered === 0) {
+      return "Your order is empty";
+    } else if (ordered > available) {
+      return "Your order is too large, we don’t have enough goods.";
+    } else {
+      return "Your order is accepted";
+    }
+  }
+  
+  // Тестові виклики:
+  console.log(checkOrder(10, 0));  // "Your order is empty"
+  console.log(checkOrder(10, 12)); // "Your order is too large..."
+  console.log(checkOrder(10, 5));  // "Your order is accepted"
+  
