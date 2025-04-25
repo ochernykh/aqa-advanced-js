1cd2c28087b8f93984d41a8e2aff54d075ac157f
ochernykh <0595887@gmail.com>
2025-04-25 07:56:26 +0300

    Add 09_promises homework tasks 01–03

---
+++ homework/09_promises/task02.js
@@ -0,0 +1,27 @@
+function getTodo() {
+    return fetch("https://jsonplaceholder.typicode.com/todos/1").then(
+        (response) => response.json()
+    );
+}
+
+function getUser() {
+    return fetch("https://jsonplaceholder.typicode.com/users/1").then(
+        (response) => response.json()
+    );
+}
+
+Promise.all([getTodo(), getUser()])
+    .then(([todo, user]) => {
+        console.log("Promise.all:", { todo, user });
+    })
+    .catch((error) => {
+        console.error("Error in Promise.all:", error);
+    });
+
+Promise.race([getTodo(), getUser()])
+    .then((result) => {
+        console.log("Promise.race:", result);
+    })
+    .catch((error) => {
+        console.error("Error in Promise.race:", error);
+    });
